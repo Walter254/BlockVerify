bb62635cbfb44a7365f2e94754c20966dc524df1
Walter <53183305+Walter254@users.noreply.github.com>
2024-04-12 15:57:54 -0400

    Update ArticleRegister.sol

--- contracts/ArticleRegister.sol
+++ contracts/ArticleRegister.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 contract ArticleRegister {
     struct Article {
-        string hash;
+        string hash; // keep track
         string metadata;
         uint256 timestamp;
     }
